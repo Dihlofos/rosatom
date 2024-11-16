1553caf9183e0ab26e849800b6572164175b61a6
Samsonov Egeny <evgeniy.samsonov@sbermegamarket.ru>
2024-11-16 13:45:17 +0300

    init commit

---
+++ Readme.md
@@ -0,0 +1,10 @@
+### Rosatom booth at Congress 2024 
+
+Simple landing page with simple gulp build
+
+`npm run start` for start developing
+`npm run build` for build
+
+To see website go to [link](https://dihlofos.github.io/rosatom/build/)
+
+production [link](https://росатом.москва)
---
+++ source/js/main/tabs.js
@@ -0,0 +1,33 @@
+"use strict";
+(function () {
+  const tabsContainer = document.querySelector(".js-tabs");
+  const contentContainer = document.querySelector(".js-content");
+  const contentITems = contentContainer.querySelectorAll(".js-content-item");
+  const tabs = tabsContainer.querySelectorAll(".js-tab");
+  const activeClass = "active";
+
+  tabs.forEach((tab) => {
+    tab.addEventListener("click", () => {
+      if (tab.classList.contains(activeClass)) return;
+      console.log("tab", tab.dataset.lang);
+      clearTabs();
+      tab.classList.add(activeClass);
+
+      const contentToshow = contentContainer.querySelector(
+        `[data-lang=${tab.dataset.lang}]`
+      );
+
+      contentToshow.classList.add(activeClass);
+    });
+  });
+
+  function clearTabs() {
+    tabs.forEach((tab) => {
+      tab.classList.remove(activeClass);
+    });
+
+    contentITems.forEach((item) => {
+      item.classList.remove(activeClass);
+    });
+  }
+})();
